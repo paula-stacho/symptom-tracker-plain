4d4a9af4988cc9b9515735112d5dd7c1fffe269a
P.S <paula.stachova@tutanota.com>
2023-02-26 11:56:57 +0100

    fix heatmap

--- src/components/Board/Heatmap.tsx
+++ src/components/Board/Heatmap.tsx
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 import Color from '../../utils/colorScheme';
 import { softBox } from '../../utils/styles';
 import { IEntry, ISymptom } from '../../utils/types';
-import { timestampToDate } from '../../utils/helpers';
+import { getSymptomLabels, timestampToDate } from '../../utils/helpers';
 
 interface IHeatmapProps {
 	entries: IEntry[];
@@ -12,6 +12,46 @@ interface IHeatmapProps {
 
 const height = 200;
 const width = 400;
+const margin = {
+	left: 80,
+	right: 20,
+	top: 20,
+	bottom: 20,
+}
+
+const getData = ({ 
+	entries,
+	knownSymptoms,
+}: {
+	entries: IEntry[];
+	knownSymptoms: ISymptom[];
+}) => {
+	const yLabels = knownSymptoms.map(({ label }) => label);
+	const xLabels: string[] = [];
+
+	const indexedData = entries.reduce((collection, { timestamp, symptomId }) => {
+		const date = timestampToDate(timestamp);
+		if (!xLabels.includes(date)) xLabels.push(date);
+		if (!collection[date]) collection[date] = {};
+		if (!collection[date][symptomId]) collection[date][symptomId] = 0;
+		collection[date][symptomId]++;
+		return collection;
+	}, {} as Record<string, Record<string, number>>);
+
+	let maxCount = 0;
+	const symptomLabels = getSymptomLabels(knownSymptoms);
+	const heatmapData: [string, string, number][] = [];
+	for (let date of Object.keys(indexedData)) {
+		for (let symptomId of Object.keys(indexedData[date])) {
+			const count = indexedData[date][symptomId];
+			if (count > maxCount) maxCount = count;
+			const item: [string, string, number] = [date, symptomLabels[symptomId], count];
+			heatmapData.push(item);
+		}
+	}
+
+	return { heatmapData, yLabels, xLabels, maxCount };
+}
 
 const Heatmap: FC<IHeatmapProps> = ({ entries, knownSymptoms }) => {
 	const d3Container = useRef(null);
@@ -19,89 +59,16 @@ const Heatmap: FC<IHeatmapProps> = ({ entries, knownSymptoms }) => {
 	useEffect(() => {
 		if (!knownSymptoms || !knownSymptoms.length) { return; }
 
-		const svg = d3.select(d3Container.current);
+		const svg = d3.select(d3Container.current).selectChild('g');
 
-		const yLabels = knownSymptoms.map(({ id }) => id);
-		const xLabels: string[] = [];
-
-		const indexedData = entries.reduce((collection, { timestamp, symptomId }) => {
-			const date = timestampToDate(timestamp);
-			if (!xLabels.includes(date)) xLabels.push(date);
-			if (!collection[date]) collection[date] = {};
-			if (!collection[date][symptomId]) collection[date][symptomId] = 0;
-			collection[date][symptomId]++;
-			return collection;
-		}, {} as Record<string, Record<string, number>>);
-
-		console.log(indexedData);
-
-		let maxCount = 0;
-		const heatmapData: [string, string, number][] = [];
-		for (let date of Object.keys(indexedData)) {
-			for (let symptomId of Object.keys(indexedData[date])) {
-				const count = indexedData[date][symptomId];
-				if (count > maxCount) maxCount = count;
-				const item: [string, string, number] = [date, symptomId, count];
-				heatmapData.push(item);
-			}
-		}
-
-		const data: [string, string, number][] = [
-			[
-				"2023-2-25",
-				"l8GT0T0qH7pZOTuoKzp1",
-				1
-			],
-			[
-				"2023-2-25",
-				"FtU1pUO5W7BxR5Gtx5tx",
-				1
-			],
-			[
-				"2023-2-25",
-				"GE7vEqq6c2pbBoIGjmB0",
-				2
-			],
-			[
-				"2023-2-26",
-				"l8GT0T0qH7pZOTuoKzp1",
-				2
-			],
-			[
-				"2023-2-26",
-				"FtU1pUO5W7BxR5Gtx5tx",
-				1
-			],
-			[
-				"2023-2-26",
-				"GE7vEqq6c2pbBoIGjmB0",
-				3
-			],
-			[
-				"2023-2-27",
-				"l8GT0T0qH7pZOTuoKzp1",
-				3
-			],
-			[
-				"2023-2-27",
-				"FtU1pUO5W7BxR5Gtx5tx",
-				2
-			],
-			[
-				"2023-2-27",
-				"GE7vEqq6c2pbBoIGjmB0",
-				2
-			]
-		];
-
-		xLabels.push('2023-2-26', '2023-2-27');
+		const { yLabels, xLabels, heatmapData, maxCount } = getData({ entries, knownSymptoms });
 
 		const x = d3.scaleBand()
 			.range([ 0, width ])
 			.domain(xLabels)
 			.padding(0.01);
 		svg.append('g')
-			.attr('transform', 'translate(0,' + height + ')')
+			.attr('transform', `translate(0, ${height})`)
 			.call(d3.axisBottom(x));
 
 		const y = d3.scaleBand()
@@ -109,39 +76,32 @@ const Heatmap: FC<IHeatmapProps> = ({ entries, knownSymptoms }) => {
 		.domain(yLabels)
 		.padding(0.01);
 		svg.append('g')
-		.call(d3.axisLeft(y));
-
-		console.log('GET X', x('2023-2-25'), 'GET Y', y('l8GT0T0qH7pZOTuoKzp1'));
+			.call(d3.axisLeft(y));
 
 		const color = d3.scaleLinear<string, string>()
 			.range(['white', Color.SECONDARY])
-			.domain([1, maxCount]);
-
-		console.log({ heatmapData });
-
-		
-
-			console.log('bandwitdhs', x.bandwidth(), y.bandwidth());
+			.domain([0, maxCount]);
 
 		svg.selectAll()
-      .data(data, function([date, symptom, count]) {return date + ':' + symptom;})
+      .data(heatmapData, ([date, symptom, count]) => `${date}:${symptom}`)
       .enter()
       .append('rect')
-      .attr('x', function([date, symptom, count]) { return x(date) })
-      .attr('y', function([date, symptom, count]) { return y(symptom) })
+      .attr('x', ([date, symptom, count]) => x(date))
+      .attr('y', ([date, symptom, count]) => y(symptom))
       .attr('width', x.bandwidth() )
       .attr('height', y.bandwidth() )
-      .style('fill', function([date, symptom, count]) { return color(count)} )
-
+      .style('fill', ([date, symptom, count]) => color(count) )
 	}, [entries, knownSymptoms]);
 
 		return (
 			<div style={ContainerStyle}>
 				<svg
-					width={width}
-					height={height}
+					width={width + margin.left + margin.right}
+					height={height + margin.top + margin.bottom}
 					ref={d3Container}
-				/>
+				>
+					<g transform={`translate(${margin.left}, ${margin.top})`} />
+				</svg>
 			</div>
 		)
 };
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,4 @@
+import { ISymptom } from './types';
 export const timestampToDateAndTime = (timestamp: number) => {
 	const date = new Date(timestamp);
 	return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
@@ -7,3 +8,9 @@ export const timestampToDate = (timestamp: number) => {
 	const date = new Date(timestamp);
 	return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 };
+
+export const getSymptomLabels = (symptoms: ISymptom[]) => 
+	symptoms.reduce((collection, { id, label }) => {
+		collection[id] = label;
+		return collection;
+	}, {} as Record<string, string>);
